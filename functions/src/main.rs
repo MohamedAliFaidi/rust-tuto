5484a1479274340a782d8ab9bc979d0201a8efc3
MohamedAliFaidi <dalyyy3259@gmail.com>
2023-08-27 11:51:59 +0100

    functions in rust

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    println!("FUNCTIONS");
+    print_excited("dvlynsk".to_string());
+
+    last_char(String::from("Dvlynsk"));
+}
+
+fn exclaim(input: String) -> String {
+    let mut output = input.to_uppercase();
+    output.push_str("!");
+    output
+}
+
+fn print_excited(input: String) -> () {
+    let output = exclaim(input);
+    println!("{}", output);
+}
+
+fn last_char(input: String) -> char {
+    if input.is_empty() {
+        print!("Empty string");
+        return 'z';
+    }
+    print!("{}", input.chars().next_back().unwrap());
+    input.chars().next_back().unwrap()
 }
